74c26339909e6458302fa141bbf1c7d182326387
zahrasalsabila26 <zahrasalsabila26@gmail.com>
2021-11-22 16:17:01 +0700

    update 3

--- src/connection.php
+++ src/connection.php
@@ -1,12 +1,17 @@
 <?php
-
+ob_start();
 getenv('MYSQL_DBHOST') ? $db_host=getenv('MYSQL_DBHOST') : $db_host="localhost";
 getenv('MYSQL_DBPORT') ? $db_port=getenv('MYSQL_DBPORT') : $db_port=3306;
 getenv('MYSQL_DBUSER') ? $db_user=getenv('MYSQL_DBUSER') : $db_user="root";
 getenv('MYSQL_DBPASS') ? $db_pass=getenv('MYSQL_DBPASS') : $db_pass="";
 getenv('MYSQL_DBNAME') ? $db_name=getenv('MYSQL_DBNAME') : $db_name="logintst";
 
-$conn = mysqli_connect($db_host,$db_port, $db_user, $db_pass, $db_name);
+$conn = mysqli_connect("$db_host", 
+"$db_port", 
+"$db_user", 
+"$db_pass", 
+"$db_name"
+);
 
 if ($conn->connect_error) 
 	die("Connection failed: " . $conn->connect_error);
